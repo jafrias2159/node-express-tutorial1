451a7196b3b4c0ae7bee117caf25615dd058d151
Jorge Frias <jorge.frias@orioninc.com>
2023-12-28 12:24:55 -0600

    Adding post endpoint for channels

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
 const PORT = 5000;
 
+const channels = [];
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.json({ hello: 'World' });
+});
+
+server.get('/hello', (req, res) => {
+  res.json({ hello: 'Future senior software developer' });
+});
+
+server.post('/api/channels', (req, res) => {
+  const channelInfo = req.body;
+  channelInfo.id = shortid.generate();
+  channels.push({ ...channelInfo });
+  res.status(201).json(channelInfo);
+});
+
 server.listen(PORT, () => {
-  console.log(`Server Running on http://localhost:${PORT}}`);
+  console.log(`*** Server Running on http://localhost:${PORT}`);
 });
